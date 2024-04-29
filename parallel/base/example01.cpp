3d95655b5e753cb99cd1e98776b2bdb482c38243
Pedro Perez <pperezm@tec.m>
2024-04-29 16:04:54 -0600

    updating example01.cpp

--- parallel/base/example01.cpp
+++ parallel/base/example01.cpp
@@ -1,3 +1,18 @@
+// =================================================================
+//
+// File: example01.cpp
+// Author: Pedro Perez
+// Description: This file implements the addition of two vectors. 
+//				The time this implementation takes will be used as 
+//				the basis to calculate the improvement obtained with 
+//				parallel technologies.
+//
+// Copyright (c) 2024 by Tecnologico de Monterrey.
+// All Rights Reserved. May be reproduced for any non-commercial
+// purpose.
+//
+// =================================================================
+
 #include <iostream>
 #include <iomanip>
 #include <chrono>
@@ -6,47 +21,48 @@
 using namespace std;
 using namespace std::chrono;
 
-const int SIZE = 100000000; // 1e8
+#define SIZE 10000000 // 1e7
 
-void add_vector(int *C, int *A, int *B, int size) {
-    for (int i = 0; i < size; i++) {
-        C[i] = A[i] + B[i];
-    }
+void add_vector(int *result, int *a, int *b, int size) {
+	for (int i = 0; i < size; i++) {
+		result[i] = a[i] + b[i];
+	}
 }
 
 int main(int argc, char* argv[]) {
-    int *A, *B, *C;
+	int *a, *b, *c;
 
-    // These variable are used to keep track of the execution time
-    high_resolution_clock::time_point start, end;
-    double timeElapsed;
+	// These variables are used to keep track of the execution time.
+	high_resolution_clock::time_point start, end;
+	double timeElapsed;
 
-    A = new int[SIZE];
-    B = new int[SIZE];
-    C = new int[SIZE];
+	a = new int [SIZE];
+	b = new int [SIZE];
+	c = new int [SIZE];
 
-    fill_array(A, SIZE);
-    display_array("A:", A);
-    fill_array(B, SIZE);
-    display_array("B:", B);
+	fill_array(a, SIZE);
+	display_array("a:", a);
+	fill_array(b, SIZE);
+	display_array("b:", b);
 
-    cout << "Starting...\n";
-    timeElapsed = 0;
-    for (int j = 0; j < N; j++) {
-        start = high_resolution_clock::now();
+	cout << "Starting...\n";
+	timeElapsed = 0;
+	for (int j = 0; j < N; j++) {
+		start = high_resolution_clock::now();
 
-        add_vector(C, A, B, SIZE);
+		add_vector(c, a, b, SIZE);
 
-        end = high_resolution_clock::now();
+		end = high_resolution_clock::now();
+		timeElapsed += 
+			duration<double, std::milli>(end - start).count();
+	}
+	display_array("c:", c);
+	cout << "avg time = " << fixed << setprecision(3) 
+		 << (timeElapsed / N) <<  " ms\n";
 
-        timeElapsed += duration<double, std::milli>(end - start).count();
-    }
-    display_array("C:", C);
-    cout << "avg time = " << fixed << setprecision(3) << (timeElapsed/N) << " ms\n";
+	delete [] a;
+	delete [] b;
+	delete [] c;
 
-    delete [] A;
-    delete [] B;
-    delete [] C;
-
-    return 0;
+	return 0;
 }
