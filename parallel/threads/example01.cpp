68608396ac0427b6f803a1fe5c2d75699b3242cb
Pedro Perez <pperezm@tec.mx>
2024-07-08 13:07:20 -0600

    Update example01.cpp

--- parallel/threads/example01.cpp
+++ parallel/threads/example01.cpp
@@ -3,8 +3,8 @@
 // File: example01.cpp
 // Author: Pedro Perez
 // Description: This file implements the addition of two vectors 
-//				using POSIX threads. To compile:
-//				g++ -o app --pthread example01.cpp
+//		using POSIX threads. To compile:
+//		g++ -o app --pthread example01.cpp
 //
 // Copyright (c) 2023 by Tecnologico de Monterrey.
 // All Rights Reserved. May be reproduced for any non-commercial
