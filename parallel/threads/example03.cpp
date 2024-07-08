b239c2f4593b4e54d9d690a29313be73e9fa9978
Pedro Perez <pperezm@tec.mx>
2024-07-08 13:07:45 -0600

    Update example03.cpp

--- parallel/threads/example03.cpp
+++ parallel/threads/example03.cpp
@@ -4,7 +4,7 @@
 // Author: Pedro Perez
 // Description: This file implements the multiplication of a matrix
 //		by a vector using POSIX threads. To compile:
-//		g++ -o app -pthread example01.cpp
+//		g++ -o app -pthread example03.cpp
 //
 // Copyright (c) 2023 by Tecnologico de Monterrey.
 // All Rights Reserved. May be reproduced for any non-commercial
